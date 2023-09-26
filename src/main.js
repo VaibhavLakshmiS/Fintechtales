7740851e90fccc0cc40599dd7468d73e292969e6
VaibhavLakshmiS <114008903+VaibhavLakshmiS@users.noreply.github.com>
2023-09-25 22:14:22 -0400

    getting there to move onto next topic !

--- src/main.js
+++ src/main.js
@@ -40,29 +40,42 @@
 // };
 
 
-window.onload = function() {
-    const coin = document.querySelector('.coin img');
-    const toolbar = document.querySelector('.toolbar');
+// window.onload = function() {
+//     const coin = document.querySelector('.coin img');
+//     const toolbar = document.querySelector('.toolbar');
 
-    // Function to zoom out the coin and move it directly to the toolbar's vertical position
-    function moveCoinToToolbar() {
-        coin.style.transition = 'all 2s ease';
-        coin.style.transform = 'scale(0.2) translate(-50%, -50%)';  // Zoom out the coin
-        coin.style.top = '30px';  // Move it to the toolbar height
-    }
+//     // Function to zoom out the coin and move it directly to the toolbar's vertical position
+//     function moveCoinToToolbar() {
+//         coin.style.transition = 'all 2s ease';
+//         coin.style.transform = 'scale(0.2) translate(-50%, -50%)';  // Zoom out the coin
+//         coin.style.top = '30px';  // Move it to the toolbar height
+//     }
 
-    // Function to hide the big coin and show the toolbar after the zoom out effect
-    function showToolbar() {
-        coin.style.display = 'none';   // Hide the big coin
-        toolbar.style.opacity = '1';   // Show the toolbar
-    }
+//     // Function to hide the big coin and show the toolbar after the zoom out effect
+//     function showToolbar() {
+//         coin.style.display = 'none';   // Hide the big coin
+//         toolbar.style.opacity = '1';   // Show the toolbar
+//     }
 
-    // Set a timeout for the coin's movement after your desired initial animations
-    setTimeout(() => {
-        moveCoinToToolbar();
+//     // Set a timeout for the coin's movement after your desired initial animations
+//     setTimeout(() => {
+//         moveCoinToToolbar();
 
-        // After the coin zooms out and moves to the toolbar position, show the toolbar
-        setTimeout(showToolbar, 2000);  // 2 seconds for zoom out
+//         // After the coin zooms out and moves to the toolbar position, show the toolbar
+//         setTimeout(showToolbar, 2000);  // 2 seconds for zoom out
 
-    }, 5000);  // Adjust this time based on your previous animations (e.g., spin duration)
-};
+//     }, 5000);  // Adjust this time based on your previous animations (e.g., spin duration)
+// };
+document.addEventListener('DOMContentLoaded', function() {
+    // After your previous code for coin animation...
+
+    setTimeout(function() {
+        document.querySelector('.toolbar').style.opacity = '1';
+        
+        // New: Make the project cards visible
+        const projectCards = document.querySelectorAll('.project-card');
+        projectCards.forEach(card => {
+            card.style.opacity = '1';
+        });
+    }, 5000);  // This is assuming your coin animation takes 4 seconds; adjust as needed.
+});
